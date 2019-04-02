00e6ef96469cd7961c3528ca7bc794d3e5b48eaf
tomlibo <koushik1992rintu@gmail.com>
2019-04-02 18:47:49 +0530

    sticker home created

--- app/src/main/java/com/tomlibo/stickerapp/MainActivity.kt
+++ app/src/main/java/com/tomlibo/stickerapp/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.tomlibo.stickerapp
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tomlibo.stickerhub.StickerHomeActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, StickerHomeActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ stickerhub/src/main/java/com/tomlibo/stickerhub/adapter/StickerAdapter.kt
@@ -0,0 +1,32 @@
+package com.tomlibo.stickerhub.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.tomlibo.stickerhub.R
+import kotlinx.android.synthetic.main.list_item_sticker.view.*
+import java.util.*
+
+class StickerAdapter(private val context: Context, private val items: ArrayList<String>) : RecyclerView.Adapter<StickerAdapter.StickerViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StickerViewHolder {
+        return StickerViewHolder(LayoutInflater.from(context).inflate(R.layout.list_item_sticker, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: StickerViewHolder, position: Int) {
+        Picasso.get()
+                .load(items.get(position))
+                .into(holder.ivSticker)
+    }
+
+    class StickerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val ivSticker = view.ivSticker
+    }
+}
